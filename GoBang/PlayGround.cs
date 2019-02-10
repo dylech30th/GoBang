bcb196c62e3f55ad81678fad5fe28bfd8377fbe1
Rinacm <2653221698@qq.com>
2019-02-10 12:20:52 +0800

    conflict detector & out of bounds detector

--- GoBang/PlayGround.cs
+++ GoBang/PlayGround.cs
@@ -51,19 +51,13 @@ namespace GoBang
         public void WhiteGo(int x, int y)
         {
             IsWhiteGo = false;
-            if (_chessBoard[x, y].Chessman == Chessman.Empty)
-            {
-                _chessBoard[x, y] = new Point(x, y, Chessman.White);
-            }
+            _chessBoard[x, y] = new Point(x, y, Chessman.White);
         }
 
         public void BlackGo(int x, int y)
         {
             IsWhiteGo = true;
-            if (_chessBoard[x, y].Chessman == Chessman.Empty)
-            {
-                _chessBoard[x, y] = new Point(x, y, Chessman.Black);
-            }
+            _chessBoard[x, y] = new Point(x, y, Chessman.Black);
         }
 
         public bool CheckChessBoardIsFull()
@@ -77,19 +71,35 @@ namespace GoBang
 
         public bool Go(int x, int y)
         {
+            if (x > 14 || y > 14)
+            {
+                Console.WriteLine("超出棋盘");
+                return false;
+            }
+
             if (CheckChessBoardIsFull())
             {
                 Console.WriteLine("和棋");
+                return true;
             }
+
+            if (_chessBoard[x, y].Chessman != Chessman.Empty)
+            {
+                Console.WriteLine("已经有棋子了");
+                return false;
+            }
+
             if (IsWhiteGo)
                 WhiteGo(x, y);
             else
                 BlackGo(x, y);
+
             if (Check(_chessBoard.GetPoint(x, y)) == Winner.Black)
             {
                 Console.WriteLine("黑方获胜");
                 return true;
             }
+
             if (Check(_chessBoard.GetPoint(x, y)) == Winner.White)
             {
                 Console.WriteLine("白方获胜");
@@ -106,19 +116,19 @@ namespace GoBang
             {
                 Console.WriteLine(playGround.GetChessBoard());
                 Console.WriteLine(playGround.IsWhiteGo ? "白方走子" : "黑方走子");
+
                 Console.WriteLine("请输入X");
                 var x = Console.ReadLine();
                 Console.WriteLine("请输入Y");
                 var y = Console.ReadLine();
+
                 if (!x.IsNumber() || !y.IsNumber() || x == null || y == null)
                 {
                     Console.WriteLine("输入格式非法");
                     continue;
                 }
-                if (playGround.Go(int.Parse(x), int.Parse(y)))
-                {
-                    break;
-                }
+
+                if (playGround.Go(int.Parse(x), int.Parse(y))) break;
             }
             Console.WriteLine("游戏结束");
             Console.ReadKey();
